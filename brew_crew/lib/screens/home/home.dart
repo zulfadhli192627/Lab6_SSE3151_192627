fd5d7db71a89a83e9017757c7000915123d4ac5b
Imdeadbeat <zul.joey99@gmail.com>
2020-11-25 17:22:05 +0800

    Half Way Done

---
+++ brew_crew/lib/screens/home/home.dart
@@ -0,0 +1,27 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Brew Crew'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Log Out'),
+            onPressed: () async {
+              await _auth.signout();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
